27905b727463aa2495c364a752767916fa65a574
Lucas <ballistickey@gmail.com>
2025-10-08 03:07:23 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      '6000-firebase-studio-1759886637461.cluster-r7kbxfo3fnev2vskbkhhphetq6.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
